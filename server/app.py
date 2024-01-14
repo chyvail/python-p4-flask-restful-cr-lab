0fa46eb630ba34b9120a44d3b296e48f198557c5
joshua chivile <joshuachivile@gmail.com>
2024-01-14 04:53:40 +0300

    working with react, post and get working

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,10 +17,52 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict = [p.to_dict() for p in Plant.query.all()]
 
+        #print(plants_dict)
+
+        response = make_response(
+            jsonify(plants_dict),
+            200
+        )
+
+        return response
+    
+    def post(self):
+        new_plant = Plant(
+            name = request.form['name'],
+            image = request.form['image'],
+            price = request.form['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201
+        )
+
+        return response
+
+api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+
+        plants_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(plants_dict),
+            200
+        )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
         
 
 if __name__ == '__main__':
